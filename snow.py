65e83cdd04db04277eef39bcb19b0a1c698e1658
jtoy <jtoy@jtoy.net>
2017-12-28 18:43:01 -0800

    snow

---
+++ snow.py
@@ -0,0 +1,15 @@
+import pybullet as p
+import random
+p.connect(p.GUI)
+p.setGravity(0,0,-0.001)
+p.setRealTimeSimulation(0)
+objects = {}
+def create_snow():
+  for i in range(50):
+    objects[i] = p.createCollisionShape(p.GEOM_SPHERE)
+    p.changeVisualShape(objects[i],-1,rgbaColor=[random.random(),random.random(),0,1])
+    p.createMultiBody(random.randint(1,10),objects[i],-1,basePosition=[random.randint(-10,10),random.randint(-10,10),random.randint(2,5)])
+for i in range(1000000000):
+  p.stepSimulation()
+  if i % 100000 == 0: 
+    create_snow()
